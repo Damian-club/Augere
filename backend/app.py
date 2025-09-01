92516a8b66727fdac19b242336d47f1cf8db1949
Greyberet <58717769+Greyberet@users.noreply.github.com>
2025-08-31 20:31:19 -0500

    small refactor

--- backend/app.py
+++ backend/app.py
@@ -2,21 +2,37 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from fastapi.exceptions import HTTPException
+
+from starlette import status
+
 import os
 
 app = FastAPI()
 
 build_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../frontend"))
 
+i: int = 0
+
+@app.get('/hello')
+async def lol():
+    global i
+    i += 1
+    return {
+        "counter": i
+    }
+
 app.mount(
     "/static",
-    StaticFiles(directory=os.path.join(build_path, ".")),
+    StaticFiles(directory=build_path),
     name="static"
 )
 
-@app.get("/{full_path:path}")
-async def serve_react(full_path: str):
-    index_file = os.path.join(build_path, "index.html")
-    if os.path.exists(index_file):
-        return FileResponse(index_file)
-    raise HTTPException(status_code=404, detail="Couln't locate index.html")
+@app.get('/')
+async def serve():
+    index = os.path.join(build_path, "index.html")
+    if os.path.exists(index):
+        return FileResponse(index)
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Not found index.html"
+    )
