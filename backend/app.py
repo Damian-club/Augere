05a2c9f9bfbfcd50fa715399f0adb98ea429f24c
Adrian Vargas <adrianfelipevr@gmail.com>
2025-10-26 16:37:27 -0500

    Adicionando servicios de logue, logout y registrarse

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,8 @@ from core.db import Base, engine
 
 from routers.user import user_router
 from routers.auth import router as auth_router
+# Importar el middlewae para solucionar problemas de los CORS
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def start(instance: FastAPI):
@@ -14,6 +16,15 @@ async def start(instance: FastAPI):
     # Cerrar
 
 app = FastAPI(lifespan=start)
+
+# Configuración de los CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # En desarrollo = *, en Produccion = URL del frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(user_router)
