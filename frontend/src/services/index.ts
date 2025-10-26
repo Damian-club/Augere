7fbce37026762611cb82277a97a074fe9e9ecdea
Adrian Vargas <adrianfelipevr@gmail.com>
2025-10-25 20:37:30 -0500

    Agregando schemas de los modelos backend

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,4 +1,4 @@
 import { environment } from "../config/environment";
-import { ContactService } from "./contact";
+import { ContactService } from "./contactService";
 
 export const contactService = new ContactService(environment.api);
