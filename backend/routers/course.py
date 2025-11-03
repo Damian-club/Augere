8a24cca77385f7f2159167dbf7df22322210cdd1
Greyberet <58717769+Greyberet@users.noreply.github.com>
2025-11-03 00:38:58 -0500

    Course fix router

--- backend/routers/course.py
+++ backend/routers/course.py
@@ -23,15 +23,17 @@ from schemas.message import Message
 #-------------------------------------
 
 # SERVICES ---------------------------
-from services.course import create_course as s_create_course
-from services.course import update_course as s_update_course
-from services.course import list_user_tutored_courses as s_list_user_tutored_courses
-from services.course import list_user_enrolled_courses as s_list_user_enrolled_courses
-from services.course import delete_course as s_delete_course
-from services.course import get_private_summary_course as s_get_private_summary_course
-from services.course import get_public_summary_course as s_get_public_summary_course
-from services.course import get_course as s_get_course
-from services.course import get_overview as s_get_overview
+from services.course import (
+    create_course,
+    update_course,
+    list_user_tutored_courses,
+    list_user_enrolled_courses,
+    delete_course,
+    get_private_summary_course,
+    get_public_summary_course,
+    get_course,
+    get_overview
+)
 #-------------------------------------
 
 router = APIRouter(prefix="/course", tags=["Course"])
@@ -40,7 +42,7 @@ router = APIRouter(prefix="/course", tags=["Course"])
 def r_create_course(
     course_create: CourseCreate, user: User = Depends(get_current_user), db=Depends(get_db)
 ) -> CourseOut:
-    return s_create_course(course_create, user=user, db=db)
+    return create_course(course_create, user=user, db=db)
 
 
 @router.get(
@@ -49,7 +51,7 @@ def r_create_course(
     response_model=list[PublicSummaryCourseOut]
 )
 def r_list_user_enrolled_courses(user: User = Depends(get_current_user), db=Depends(get_db)) -> list[PublicSummaryCourseOut]:
-    return s_list_user_enrolled_courses(user, db=db)
+    return list_user_enrolled_courses(user, db=db)
 
 
 @router.get(
@@ -60,7 +62,7 @@ def r_list_user_enrolled_courses(user: User = Depends(get_current_user), db=Depe
 def r_list_tutored_courses(
     user: User = Depends(get_current_user),
 ) -> list[PrivateSummaryCourseOut]:
-    return s_list_user_tutored_courses(user)
+    return list_user_tutored_courses(user)
 
 
 @router.get(
@@ -71,7 +73,7 @@ def r_list_tutored_courses(
 def r_get_overview(
     user: User = Depends(get_current_user), db=Depends(get_db)
 ) -> OverviewOut:
-    return s_get_overview(user, db=db)
+    return get_overview(user, db=db)
 
 
 @router.get(
@@ -82,7 +84,7 @@ def r_get_overview(
 def r_get_public_summary_course(
     uuid: UUID, user: User = Depends(get_current_user), db=Depends(get_db)
 ) -> PublicSummaryCourseOut:
-    return s_get_public_summary_course(uuid, user=user, db=db)
+    return get_public_summary_course(uuid, user=user, db=db)
 
 
 @router.get(
@@ -93,11 +95,11 @@ def r_get_public_summary_course(
 def r_get_private_summary_course(
     uuid: UUID, user: User = Depends(get_current_user), db=Depends(get_db)
 ) -> PrivateSummaryCourseOut:
-    return s_get_private_summary_course(uuid, user=user, db=db)
+    return get_private_summary_course(uuid, user=user, db=db)
 
 @router.get("/{uuid}", summary="Obtener un curso por UUID", response_model=CourseOut)
 def r_get_course(uuid: UUID, user: User = Depends(get_current_user), db=Depends(get_db)) -> CourseOut:
-    return s_get_course(uuid, user=user, db=db)
+    return get_course(uuid, user=user, db=db)
 
 
 @router.put("/{uuid}", summary="Actualizar un curso", response_model=CourseOut)
@@ -107,11 +109,11 @@ def r_update_course(
     user: User = Depends(get_current_user),
     db=Depends(get_db),
 ) -> CourseOut:
-    return s_update_course(uuid, course_update=course_update, user=user, db=db)
+    return update_course(uuid, course_update=course_update, user=user, db=db)
 
 
 @router.delete("/{uuid}", summary="Eliminar un curso", response_model=Message)
 def r_delete_course(
     uuid: UUID, user: User = Depends(get_current_user), db=Depends(get_db)
 ) -> Message:
-    return s_delete_course(uuid, user=user, db=db)
+    return delete_course(uuid, user=user, db=db)
